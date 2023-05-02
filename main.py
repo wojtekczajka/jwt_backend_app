71ee4f75fb1de43dc318abf34af9491103eae6e5
wojtekczajka <w.czajka@gmail.com>
2023-05-02 14:52:24 +0200

    update

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8080"],
+    allow_origins=["https://main.dmke1hvp7g7q1.amplifyapp.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
