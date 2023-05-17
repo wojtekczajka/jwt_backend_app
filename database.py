596add34e78e7ad5bb3c811704cd20d44964c5cc
wojtekczajka <w.czajka@gmail.com>
2023-05-17 14:12:40 +0200

    update

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://admin:!Bioshock13!@database-1.cqyfd9wssou1.eu-north-1.rds.amazonaws.com:3306/jwt_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://admin:!Bioshock13!@database-1.cqyfd9wssou1.eu-north-1.rds.amazonaws.com:3306/jwt_db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
