fbdc9bb6b1877a1fe75ffbf6f2b2d75953d81696
wojtekczajka <w.czajka@gmail.com>
2023-06-10 11:34:18 +0200

    cors update.

--- main.py
+++ main.py
@@ -36,7 +36,12 @@ oauth.register(
     }
 )
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://main.d3f9gvqybmfju1.amplifyapp.com",
+                 "http://main.d3f9gvqybmfju1.amplifyapp.com",
+                 "http://127.0.0.1:8000/",
+                 "http://127.0.0.1:8080/",
+                 "https://ti4r36gvwlegcokae4ofeivnva0hwiqn.lambda-url.eu-north-1.on.aws"
+                 ]
 
 app.add_middleware(
     CORSMiddleware,
